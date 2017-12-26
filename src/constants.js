02866c81c3816502253fa2612cdec34798282a7a
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-12-26 22:47:52 +1100

    update UI of moves list

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,25 @@
 const moveTypeColors = {
-  toprock: 'magenta',
-  rocking: 'red',
-  drop: 'volcano',
-  footwork: 'orange',
-  floorwork: 'gold',
-  backrock: 'lime',
-  powermove: 'cyan',
-  freeze: 'green',
-  position: 'blue'
+  toprock: 'rgb(255,163,204)',
+  rocking: 'rgb(255,152,148)',
+  drop: 'rgb(255,178,139)',
+  footwork: 'rgb(255,207,134)',
+  floorwork: 'rgb(255,225,132)',
+  backrock: 'rgb(231,255,132)',
+  powermove: 'rgb(174,232,132)',
+  freeze: 'rgb(124,229,217)',
+  position: 'rgb(134,207,255)'
 };
 
-export { moveTypeColors };
+const moveTypeShortNames = {
+  toprock: "Top",
+  rocking: "Rock",
+  drop: "Drop",
+  footwork: "Foot",
+  floorwork: "Floor",
+  backrock: "Back",
+  powermove: "Power",
+  freeze: "Freeze",
+  position: "Pos"
+}
+
+export { moveTypeColors, moveTypeShortNames };
--- src/containers/MovesList.js
+++ src/containers/MovesList.js
@@ -1,8 +1,10 @@
 import { config } from '../config'
+import { moveTypeColors, moveTypeShortNames } from '../constants'
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
 import axios from 'axios';
-import { Button } from 'antd';
-import MovesListItem from '../components/MovesListItem';
+import _ from 'lodash';
+import { Avatar, List } from 'antd';
 
 class MovesList extends Component {
   constructor(props) {
@@ -10,11 +12,17 @@ class MovesList extends Component {
 
     this.state = {
       moves: [],
+      redirect: false,
+      redirectUrl: ''
     };
 
     this.getMoves = this.getMoves.bind(this);
-    this.resetMoves = this.resetMoves.bind(this);
+    this.redirectToUrl = this.redirectToUrl.bind(this);
   };
+
+  componentDidMount() {
+    this.getMoves();
+  }
 
   getMoves() {
     axios.get(config.API_URL + 'moves')
@@ -26,18 +34,32 @@ class MovesList extends Component {
       });
   }
 
-  resetMoves() {
-    this.setState({moves: []});
+  redirectToUrl(url) {
+    this.setState({redirect: true});
+    this.setState({redirectUrl: url})
   }
 
   render() {
-    const movesList = this.state.moves.map((move, i) => <MovesListItem key={'move-' + i} move={move} />)
-
     return (
       <div>
-        <Button onClick={this.getMoves}>Get Moves</Button>
-        <Button onClick={this.resetMoves}>Reset</Button>
-        {movesList}
+        {this.state.redirect ? <Redirect to={this.state.redirectUrl} /> : null}
+        <List
+          itemLayout="vertical"
+          dataSource={this.state.moves}
+          renderItem={move => (
+            <List.Item>
+              <div className="vertical-align" onClick={() => this.redirectToUrl('/move/' + move._id)}>
+                <Avatar size="large" style={{ backgroundColor: moveTypeColors[move.type] }}>{moveTypeShortNames[move.type]}</Avatar>
+                <div className="horizontal-spacer" />
+                <div style={{lineHeight:"125%"}}>
+                  <span className="list-title">{move.name}</span>
+                  <br />
+                  <span>{_.capitalize(move.origin)} {_.capitalize(move.type)}</span>
+                </div>
+              </div>
+            </List.Item>
+          )}
+        />
       </div>
     );
   }
