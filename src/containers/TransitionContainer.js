252a27653ab43b7ccb7c896d64b7e5ce24cffbe6
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2018-01-10 22:52:28 +1100

    add page transitions

---
+++ src/containers/TransitionContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+const TransitionContainer = Page => {
+  return props =>
+    <div className="page">
+      <ReactCSSTransitionGroup
+        transitionAppear={true}
+        transitionAppearTimeout={600}
+        transitionEnterTimeout={600}
+        transitionLeaveTimeout={200}
+        transitionName={props.match.path === '/thanks' ? 'SlideIn' : 'SlideOut'}
+      >
+        <Page {...props} />
+      </ReactCSSTransitionGroup>
+    </div>;
+};
+
+export default TransitionContainer;
