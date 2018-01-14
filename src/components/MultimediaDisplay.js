ea1600c96faaeb5aeeb8715a0366cfd1bbf82d77
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2018-01-14 17:01:51 +1100

    fix multimedia display

--- src/components/MultimediaDisplay.js
+++ src/components/MultimediaDisplay.js
@@ -3,20 +3,20 @@ import { Player } from 'video-react';
 import "video-react/dist/video-react.css";
 
 const MultimediaDisplay = props => {
-  const isVideo = (fileName) => {
-    const extension = fileName.substr(fileName.lastIndexOf('.') + 1);
-    return ['mp4', 'mov', 'avi'].includes(extension)
-  }
-
   const isImage = (fileName) => {
     const extension = fileName.substr(fileName.lastIndexOf('.') + 1);
-    return ['jpg', 'jpeg', 'png', 'gif'].includes(extension)
+    return ['jpg', 'jpeg', 'png', 'gif'].includes(extension.toLowerCase())
+  }
+
+  const isVideo = (fileName) => {
+    const extension = fileName.substr(fileName.lastIndexOf('.') + 1);
+    return ['mp4', 'mov', 'avi'].includes(extension.toLowerCase())
   }
 
   if (isImage(props.fileName) && props.visible) {
     return (
       <div className="align-center">
-        <img alt="" src={props.multimediaUrl} />
+        <img alt="" src={props.multimediaUrl} style={{maxWidth: '100%'}} />
       </div>
     )
   } else if (isVideo(props.fileName) && props.visible) {
